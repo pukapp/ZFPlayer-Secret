1c6cddc933d8525b01b7332d2d29fdee03ed0f0a
刘超 <chaosliu77@gmail.com>
2019-03-21 11:25:52 +0800

    Initial Commit

---
+++ ZFPlayer-Secret/ZFPlayer_Secret.h
@@ -0,0 +1,19 @@
+//
+//  ZFPlayer_Secret.h
+//  ZFPlayer-Secret
+//
+//  Created by 刘超 on 2019/3/21.
+//  Copyright © 2019 Secret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZFPlayer_Secret.
+FOUNDATION_EXPORT double ZFPlayer_SecretVersionNumber;
+
+//! Project version string for ZFPlayer_Secret.
+FOUNDATION_EXPORT const unsigned char ZFPlayer_SecretVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFPlayer_Secret/PublicHeader.h>
+
+
